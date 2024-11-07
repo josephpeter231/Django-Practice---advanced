cd23e750b706b89419fff5fa09ca251111791556
josephpeter231 <josephpeterjcit@gmail.com>
2024-11-07 14:08:39 +0530

    drf understanding

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -1,7 +1,7 @@
-# project/urls.py
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('main/', include('app1.urls', namespace='app_1')),  
-    path('main2/', include('newpractice.urls', namespace='newpractice')),  
+    path('admin/', admin.site.urls),
+    path('api/', include('app1.urls')),  
 ]
