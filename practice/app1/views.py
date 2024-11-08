365f473f80a2e293ca1843ba576c94709e68b92d
josephpeter231 <josephpeterjcit@gmail.com>
2024-11-08 16:06:35 +0530

    migrations learnt

--- practice/app1/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def home_view(request):
-    return Response({"message": "Welcome to the Home Page"})
--- practice/newpractice/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-  
-
-class NewpracticeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'newpractice'
